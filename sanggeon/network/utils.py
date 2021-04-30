8f9713eb2e5171de63928c4e8216309efded5001
gun <sglee487@gmail.com>
2021-04-30 18:55:02 +0900

    unet 모델 추가

--- sanggeon/network/utils.py
+++ sanggeon/network/utils.py
@@ -1,8 +1,12 @@
 from .segnet import SegNetVgg16
-
+import segmentation_models_pytorch as smp
 
 def get_model(args, num_classes):
     if args.model == 'segnetvgg16':
         model = SegNetVgg16(num_classes=num_classes)
+    if args.model == 'unetefb0':
+        model = smp.Unet(encoder_name='efficientnet-b0', classes=num_classes, encoder_weights='imagenet', activation=None)
+    if args.model == 'unetmnv2':
+        model = smp.Unet(encoder_name='mobilenet_v2', classes=num_classes, encoder_weights='imagenet', activation=None)
 
     return model
