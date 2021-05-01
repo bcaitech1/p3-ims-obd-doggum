5eb90ae31acba72fffc89f9f84f2d15718e00be1
gun <sglee487@gmail.com>
2021-05-01 23:08:12 +0900

    save 이름 변경 및 mIoU 산정방식 수정

--- sanggeon/train.py
+++ sanggeon/train.py
@@ -11,9 +11,9 @@ from tqdm import tqdm
 import pandas as pd
 
 from datasets.data_loader import setup_loader
-from utils.utils import label_accuracy_score
+from utils.utils import fast_hist, label_accuracy_score
 from datasets.data_loader import CustomDataLoader
-from transforms.Augmentations import TestAugmentation, CustomAugmentation
+from transforms.Augmentations import TestAugmentation, CustomAugmentation, CustomAugmentation2
 from visualize.showplots import showImageMask
 from network.utils import get_model
 from loss.optimizer import get_optimizer
@@ -23,13 +23,13 @@ from loss.utils import get_loss
 # Argument Parser
 parser = argparse.ArgumentParser(description='Semantic Segmentation')
 parser.add_argument('--lr', type=float, default=0.0001)
-parser.add_argument('--epochs', type=int, default=11)
-parser.add_argument('--batch_size', type=int, default=8)
+parser.add_argument('--epochs', type=int, default=20)
+parser.add_argument('--batch_size', type=int, default=16)
 parser.add_argument('--seed', type=int, default=21)
 parser.add_argument('--eval', type=bool, default=False)
 parser.add_argument('--criterion', type=str, default='cross_entropy')
 parser.add_argument('--optimizer', type=str, default='adam')
-parser.add_argument('--model', type=str, default='segnetvgg16')
+parser.add_argument('--model', type=str, default='unetmnv2')
 
 # Container environment
 parser.add_argument("--dataset_path", type=str, default= '../input/data')
@@ -78,7 +78,19 @@ def main():
 
     val_transform = CustomAugmentation()
 
-    test_transform = CustomAugmentation()
+    test_transform = TestAugmentation()
+    # from albumentations.pytorch import ToTensorV2
+    # train_transform = A.Compose([
+    #     ToTensorV2()
+    # ])
+    #
+    # val_transform = A.Compose([
+    #     ToTensorV2()
+    # ])
+    #
+    # test_transform = A.Compose([
+    #     ToTensorV2()
+    # ])
 
     # create own Dataset 1 (skip)
     # validation set을 직접 나누고 싶은 경우
@@ -103,30 +115,31 @@ def main():
                                                batch_size=args.batch_size,
                                                shuffle=True,
                                                num_workers=4,
-                                               collate_fn=collate_fn)
+                                               collate_fn=collate_fn,
+                                               drop_last = True)
 
     val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
                                              batch_size=args.batch_size,
                                              shuffle=False,
-                                             num_workers=4,
+                                             num_workers=2,
                                              collate_fn=collate_fn)
 
     test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                               batch_size=args.batch_size,
-                                              num_workers=4,
+                                              num_workers=2,
                                               collate_fn=collate_fn)
 
-    showImageMask(train_loader, list(sorted_df.Categories))
-    showImageMask(val_loader, list(sorted_df.Categories))
-    showImageMask(test_loader, list(sorted_df.Categories), test=True)
+    # showImageMask(train_loader, list(sorted_df.Categories))
+    # showImageMask(val_loader, list(sorted_df.Categories))
+    # showImageMask(test_loader, list(sorted_df.Categories), test=True)
 
     # 구현된 model에 임의의 input을 넣어 output이 잘 나오는지 test
 
     model = get_model(args, num_classes=12)
-    x = torch.randn([1, 3, 512, 512])
-    print("input shape : ", x.shape)
-    out = model(x).to(device)
-    print("output shape : ", out.size())
+    # x = torch.randn([1, 3, 512, 512])
+    # print("input shape : ", x.shape)
+    # out = model(x).to(device)
+    # print("output shape : ", out.size())
 
     model = model.to(device)
 
@@ -137,11 +150,11 @@ def main():
     if not os.path.isdir(saved_dir):
         os.mkdir(saved_dir)
 
-    criterion = get_loss(args)
-    optimizer = get_optimizer(args, model)
+    if not args.eval:
+        criterion = get_loss(args)
+        optimizer = get_optimizer(args, model)
 
-    train(args.epochs, model, train_loader, val_loader, criterion, optimizer, saved_dir, val_every, device)
-
+        train(args.epochs, model, train_loader, val_loader, criterion, optimizer, saved_dir, val_every, device)
 
     # best model 저장된 경로
     model_path = f'./saved/{args.model}_best_model.pt'
@@ -168,7 +181,7 @@ def main():
 
     # submission.csv로 저장
     tm = time.gmtime()
-    time_string = time.strftime('%yy%mm%dd%H_%M_%S', tm)
+    time_string = time.strftime('%yy%mm%dd_%H_%M_%S', tm)
     submission.to_csv(f"./submission/{args.model}_{time_string}.csv", index=False)
 
 
@@ -180,6 +193,7 @@ def save_model(model, saved_dir, file_name='SegNet_best_model.pt'):
 def train(num_epochs, model, data_loader, val_loader, criterion, optimizer, saved_dir, val_every, device):
     print('Start training..')
     best_loss = 9999999
+    best_mIoU = 0
     for epoch in range(num_epochs):
         model.train()
         for step, (images, masks, _) in tqdm(enumerate(data_loader), total=len(data_loader)):
@@ -204,6 +218,8 @@ def train(num_epochs, model, data_loader, val_loader, criterion, optimizer, save
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(
                     epoch + 1, num_epochs, step + 1, len(data_loader), loss.item()))
 
+        if (epoch + 1) == 6:
+            save_model(model, saved_dir, f'{args.model}_best_model6.pt')
         # validation 주기에 따른 loss 출력 및 best model 저장
         if (epoch + 1) % val_every == 0:
             avrg_loss = validation(epoch + 1, model, val_loader, criterion, device)
@@ -220,10 +236,12 @@ def validation(epoch, model, data_loader, criterion, device):
     with torch.no_grad():
         total_loss = 0
         cnt = 0
-        mIoU_list = []
+        n_class = 12
+        hist = np.zeros((n_class, n_class))
         for step, (images, masks, _) in enumerate(data_loader):
+
             images = torch.stack(images)  # (batch, channel, height, width)
-            masks = torch.stack(masks).long()  # (batch, channel, height, width)
+            masks = torch.stack(masks).long()  # (batch, height, width)
 
             images, masks = images.to(device), masks.to(device)
 
@@ -232,13 +250,16 @@ def validation(epoch, model, data_loader, criterion, device):
             total_loss += loss
             cnt += 1
 
-            outputs = torch.argmax(outputs, dim=1).detach().cpu().numpy()
+            outputs = torch.argmax(outputs.squeeze(), dim=1).detach().cpu().numpy()
 
-            mIoU = label_accuracy_score(masks.detach().cpu().numpy(), outputs, n_class=12)[2]
-            mIoU_list.append(mIoU)
+            # 각각의 mask에 대한 confusion matrix를 hist에 저장
+            for lt, lp in zip(outputs, masks.detach().cpu().numpy()):
+                hist += fast_hist(lt.flatten(), lp.flatten(), n_class)
 
         avrg_loss = total_loss / cnt
-        print('Validation #{}  Average Loss: {:.4f}, mIoU: {:.4f}'.format(epoch, avrg_loss, np.mean(mIoU_list)))
+        mIoU = label_accuracy_score(hist)
+        print('Validation #{}  Average Loss: {:.4f}, mIoU: {:.4f}'.format(epoch, avrg_loss,
+                                                                          mIoU))
 
     return avrg_loss
 
--- sanggeon/utils/utils.py
+++ sanggeon/utils/utils.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 
-def _fast_hist(label_true, label_pred, n_class):
+def fast_hist(label_true, label_pred, n_class):
     mask = (label_true >= 0) & (label_true < n_class)
     hist = np.bincount(
         n_class * label_true[mask].astype(int) +
@@ -10,25 +10,14 @@ def _fast_hist(label_true, label_pred, n_class):
     return hist
 
 
-def label_accuracy_score(label_trues, label_preds, n_class):
+def label_accuracy_score(hist):
     """Returns accuracy score evaluation result.
-      - overall accuracy
-      - mean accuracy
       - mean IU
-      - fwavacc
     """
-    hist = np.zeros((n_class, n_class))
-    for lt, lp in zip(label_trues, label_preds):
-        hist += _fast_hist(lt.flatten(), lp.flatten(), n_class)
-    acc = np.diag(hist).sum() / hist.sum()
-    with np.errstate(divide='ignore', invalid='ignore'):
-        acc_cls = np.diag(hist) / hist.sum(axis=1)
-    acc_cls = np.nanmean(acc_cls)
     with np.errstate(divide='ignore', invalid='ignore'):
         iu = np.diag(hist) / (
             hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist)
         )
     mean_iu = np.nanmean(iu)
-    freq = hist.sum(axis=1) / hist.sum()
-    fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
-    return acc, acc_cls, mean_iu, fwavacc
+
+    return mean_iu
