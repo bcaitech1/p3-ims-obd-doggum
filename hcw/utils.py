a5a5d1368de8c5caa7c81d12b6e3db664c3af8bf
hcw3737 <77056802+hcw3737@users.noreply.github.com>
2021-05-04 18:29:37 +0900

    Create utils.py

---
+++ hcw/utils.py
@@ -0,0 +1,70 @@
+# https://github.com/wkentaro/pytorch-fcn/blob/master/torchfcn/utils.py
+import numpy as np
+import torch
+import os
+
+def _fast_hist(label_true, label_pred, n_class):
+    mask = (label_true >= 0) & (label_true < n_class)
+    hist = np.bincount(
+        n_class * label_true[mask].astype(int) +
+        label_pred[mask], minlength=n_class ** 2).reshape(n_class, n_class)
+    return hist
+
+
+def label_accuracy_score(label_trues, label_preds, n_class):
+    """Returns accuracy score evaluation result.
+      - overall accuracy
+      - mean accuracy
+      - mean IU
+      - fwavacc
+    """
+    hist = np.zeros((n_class, n_class))
+    for lt, lp in zip(label_trues, label_preds):
+        hist += _fast_hist(lt.flatten(), lp.flatten(), n_class)
+    acc = np.diag(hist).sum() / hist.sum()
+    with np.errstate(divide='ignore', invalid='ignore'):
+        acc_cls = np.diag(hist) / hist.sum(axis=1)
+    acc_cls = np.nanmean(acc_cls)
+    with np.errstate(divide='ignore', invalid='ignore'):
+        iu = np.diag(hist) / (
+            hist.sum(axis=1) + hist.sum(axis=0) - np.diag(hist)
+        )
+    mean_iu = np.nanmean(iu)
+    freq = hist.sum(axis=1) / hist.sum()
+    fwavacc = (freq[freq > 0] * iu[freq > 0]).sum()
+    return acc, acc_cls, mean_iu, fwavacc
+
+def save_model(model, saved_dir, file_name='best_model.pt'):    #file_name = default
+    check_point = {'net': model.state_dict()}
+    output_path = os.path.join(saved_dir, file_name)
+    torch.save(model.state_dict(), output_path)
+
+def validation(epoch, model, data_loader, criterion, device):
+    print('Start validation #{}'.format(epoch))
+    model.eval()
+    with torch.no_grad():
+        total_loss = 0
+        cnt = 0
+        mIoU_sum = 0
+        for step, (images, masks, _) in enumerate(data_loader):
+            images = torch.stack(images)  # (batch, channel, height, width)
+            masks = torch.stack(masks).long()  # (batch, channel, height, width)
+
+            images, masks = images.to(device), masks.to(device)
+
+            outputs = model(images)
+            loss = criterion(outputs, masks)
+            total_loss += loss
+            cnt += 1
+
+            outputs = torch.argmax(outputs, dim=1).detach().cpu().numpy()
+
+            mIoU = label_accuracy_score(masks.detach().cpu().numpy(), outputs, n_class=12)[2]
+            mIoU_sum+=mIoU
+
+        avrg_loss = total_loss / cnt
+        avrg_mIoU = mIoU_sum / cnt
+
+        print('Validation #{}  Average Loss: {:.4f}, mIoU: {:.4f}'.format(epoch, avrg_loss,avrg_mIoU))
+
+    return avrg_loss,avrg_mIoU
