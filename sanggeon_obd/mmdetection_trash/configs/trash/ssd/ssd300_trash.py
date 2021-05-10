8e7a93bf49cd0c5db8dbdfb2eb69a9234e2cd326
gun <sglee487@gmail.com>
2021-05-10 20:52:34 +0900

    daily mission day 1

---
+++ sanggeon_obd/mmdetection_trash/configs/trash/ssd/ssd300_trash.py
@@ -0,0 +1,107 @@
+_base_ = [
+    '../../_base_/models/ssd300.py',
+    '../dataset.py',
+    '../../_base_/schedules/schedule_2x.py',
+    '../../_base_/default_runtime.py'
+]
+
+# model settings
+model = dict(
+    bbox_head=dict(
+        num_classes=11,
+    )
+)
+
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = '../../input/data/'
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[1, 1, 1], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile', to_float32=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PhotoMetricDistortion',
+        brightness_delta=32,
+        contrast_range=(0.5, 1.5),
+        saturation_range=(0.5, 1.5),
+        hue_delta=18),
+    dict(
+        type='Expand',
+        mean=img_norm_cfg['mean'],
+        to_rgb=img_norm_cfg['to_rgb'],
+        ratio_range=(1, 4)),
+    dict(
+        type='MinIoURandomCrop',
+        min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
+        min_crop_size=0.3),
+    dict(type='Resize', img_scale=(300, 300), keep_ratio=False),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(300, 300),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=False),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=8,
+    workers_per_gpu=4,
+    train=dict(
+        type=dataset_type,
+        ann_file=data_root + 'train.json',
+        img_prefix=data_root,
+        pipeline=train_pipeline),
+    val=dict(
+        type=dataset_type,
+        ann_file=data_root + 'val.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        ann_file=data_root + 'test.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline))
+
+# data = dict(
+#     samples_per_gpu=8,
+#     workers_per_gpu=3,
+#     train=dict(
+#         _delete_=True,
+#         type='CocoDataset',
+#         dataset=dict(
+#             type=dataset_type,
+#             ann_file=data_root + 'train.json',
+#             img_prefix=data_root,
+#             pipeline=train_pipeline
+#         )
+#     ),
+#     val=dict(
+#         type=dataset_type,
+#         ann_file=data_root + 'val.json',
+#         img_prefix=data_root,
+#         pipeline=test_pipeline
+#     ),
+#     test=dict(
+#         type=dataset_type,
+#         ann_file=data_root + 'test.json',
+#         img_prefix=data_root,
+#         pipeline=test_pipeline
+#     )
+#
+# )
+# optimizer
+optimizer = dict(type='SGD', lr=2e-3, momentum=0.9, weight_decay=5e-4)
+optimizer_config = dict(_delete_=True)
+
+# runner = dict(type='EpochBasedRunner', max_epochs=12)
+# evaluation = dict(interval=1, metric=['bbox'])
